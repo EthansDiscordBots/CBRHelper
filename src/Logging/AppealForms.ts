2db3d0b352ce239ccfe18896c741137fcdd0db9a
Scr1ptxd_Ethxn <154760633+Scr1ptxdEthxn@users.noreply.github.com>
2025-01-13 19:14:57 +0000

    Add mod appeal form logging

--- src/Logging/AppealForms.ts
+++ src/Logging/AppealForms.ts
@@ -7,7 +7,8 @@ interface Logging extends EntryData {
     type: string
 }
 const ms = new MarcSyncClient(String(process.env.mskey))
-const AppealFormsPending: Collection<Logging> = ms.getCollection("AppealForms")
+const AppealFormsPending: Collection<Logging> = ms.getCollection("MRAppealForms")
+const ModAppealForms: Collection<Logging> = ms.getCollection("ModAppealForms")
 import { retryOperation } from "../Functions/retry";
 
 module.exports = {
@@ -15,7 +16,7 @@ module.exports = {
     once: true,
     async execute(client) {
         ms.on("entryCreated", async (data) => {
-            if (data.getCollectionName() == "AppealForms") {
+            if (data.getCollectionName() == "MRAppealForms") {
                 const embeds = []
                 let content = ""
                 let charlength = 0
@@ -80,5 +81,71 @@ module.exports = {
                 client.channels.cache.get("1132230548576817282").send({ content: "<@&1130751888875343982>", embeds: embeds })
             }
         }
-    }
+
+        ms.on("entryCreated", async (data) => {
+            if (data.getCollectionName() == "ModAppealForms") {
+                const embeds = []
+                let content = ""
+                let charlength = 0
+                let embed = new EmbedBuilder()
+                for (let i = 0; i < data.getValue("fields").length; i++) {
+                    if (charlength < 3000) {
+                        content = content + `**${data.getValue("fields")[i].name}**` + "\n\n"
+                        content = content + data.getValue("fields")[i].value + "\n\n"
+                        charlength += String(data.getValue("fields")[i].name).length
+                        charlength += String(data.getValue("fields")[i].name).length
+                    } else {
+                        embed.setColor(0x00ffe5)
+                        embed.setDescription(content)
+                        charlength = 0
+                        content = ""
+                        embeds.push(embed)
+                        embed = new EmbedBuilder()
+                    }
+                    embed.setColor(0x00ffe5)
+                    embed.setDescription(content)
+                    embeds.push(embed)
+                    data.delete()
+                }
+                client.channels.cache.get("1328133530424840212").send({ embeds: embeds })
+            }
+        })
+        var pendingappeals
+        async function getentries() {
+            return await ModAppealForms.getEntries()
+        }
+        retryOperation(getentries)
+            .then(res => pendingappeals = (res || []))
+            .catch(err => pendingappeals = [])
+        await new Promise(r => setTimeout(r, 1000))
+        if (pendingappeals?.length > 0) {
+            for (let i = 0; i < pendingappeals.length; i++) {
+                const embeds = []
+                let content = ""
+                let charlength = 0
+                const data: Entry<Logging> = pendingappeals[i]
+                var embed = new EmbedBuilder()
+                for (let i = 0; i < data.getValue("fields").length; i++) {
+                    if (charlength < 3000) {
+                        content = content + `**${data.getValue("fields")[i].name}**` + "\n\n"
+                        content = content + data.getValue("fields")[i].value + "\n\n"
+                        charlength += String(data.getValue("fields")[i].name).length
+                        charlength += String(data.getValue("fields")[i].name).length
+                    } else {
+                        charlength = 0
+                        content = ""
+                        embed.setColor(0x00ffe5)
+                        embed.setDescription(content)
+                        embeds.push(embed)
+                        embed = new EmbedBuilder()
+                    }
+
+                }
+                embed.setColor(0x00ffe5)
+                embed.setDescription(content)
+                embeds.push(embed)
+                ModAppealForms.deleteEntryById(data.getValue("_id"))
+                client.channels.cache.get("1328133530424840212").send({  embeds: embeds })
+            }
+    }}
 }
