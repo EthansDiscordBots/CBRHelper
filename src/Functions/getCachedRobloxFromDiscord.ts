fcf5fa41aab5ba2ceb2e683d8a6af90cbc17b8c3
Scr1ptxd_Ethxn <154760633+Scr1ptxdEthxn@users.noreply.github.com>
2025-01-13 23:10:37 +0000

    yay

--- src/Functions/getCachedRobloxFromDiscord.ts
+++
@@ -1,5 +0,0 @@
-import { QuickDB } from "quick.db";
-const db = new QuickDB()
-export async function getCachedRobloxFromDiscord(DiscordId: String | Number) {
-    return await db.get(`${DiscordId}.verifiedRoblox`)
-}
--- src/Functions/getRobloxIdFromDiscord.ts
+++
@@ -1,7 +0,0 @@
-export async function getRobloxIdFromDiscord(DiscordId: number) {
-    const response = await fetch(`https://api.blox.link/v4/public/guilds/480452557949370380/discord-to-roblox/${DiscordId}`, { 
-        headers: { "Authorization": String(process.env.BloxlinkAPIKey) } 
-    });
-    const data = await response.json();
-    return data.robloxID;
-}
