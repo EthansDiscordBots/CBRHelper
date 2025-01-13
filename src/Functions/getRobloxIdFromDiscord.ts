ff2c52d43382bc6e5e347366a7664f74ba500708
Scr1ptxd_Ethxn <154760633+Scr1ptxdEthxn@users.noreply.github.com>
2025-01-13 22:58:46 +0000

    Delete src directory

--- src/Functions/getRobloxIdFromDiscord.ts
+++
@@ -1,7 +0,0 @@
-export async function getRobloxIdFromDiscord(DiscordId: number) {
-    const response = await fetch(`https://api.blox.link/v4/public/guilds/process.env.MainServerId/discord-to-roblox/${DiscordId}`, { 
-        headers: { "Authorization": String(process.env.BloxlinkAPIKey) } 
-    });
-    const data = await response.json();
-    return data.robloxID;
-}
