e9df4266e4b08e478107763de2ad9f7e10904357
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-18 16:03:05 +0000

    test

---
+++ src/Events/webhooklistener.ts
@@ -0,0 +1,14 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+
+const app = express()
+const PORT = 3000
+app.use(bodyParser.json())
+
+app.get("/test", (req, res) => {
+    const body = req.body
+
+    res.status(200).send(JSON.stringify({
+        "success": true
+    }))
+})
