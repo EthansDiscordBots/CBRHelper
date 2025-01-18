464546e7331e2567b2ba151533d10c3cec5b3e96
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-18 16:11:33 +0000

    change port

--- src/Events/webhooklistener.ts
+++ src/Events/webhooklistener.ts
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser");
 
 const app = express()
-const PORT = 3000
+const PORT = 2999
 app.use(bodyParser.json())
 
 app.get("/test", (req, res) => {
