8054fdcdc551dce265f02d5bdcb9d90f59d63896
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-19 10:46:34 +0000

    Start switch to own service from marcsync

--- index.ts
+++ index.ts
@@ -2,10 +2,32 @@ const { Client, GatewayIntentBits, Partials} = require(`discord.js`);
 const client = new Client({ intents: [GatewayIntentBits.GuildVoiceStates ,GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages], partials: [Partials.Message, Partials.Channel, Partials.Reaction] }); 
 import { deployEvents } from "./src/Functions/deployEvents";
 import { deployCommands } from "./src/Functions/deployCommands";
-import { getPlayerThumbnail } from "noblox.js";
+import { CommandLogs } from "./src/NewFunctions/CommandLoggingNEW";
+const express = require("express");
+const bodyParser = require("body-parser");
 client.setMaxListeners(100);
 require('dotenv').config();
 deployCommands(client)
 deployEvents(client)
 
 client.login(String(process.env.token))
+
+const app = express();
+const PORT = 5000; // Use any available port
+
+app.use(bodyParser.json());
+
+app.post("/command-logging", (req, res) => {""
+    if (req.headers.authorization != "wseutgghkjgigJKjklhgJHGigsfguaKgdjagLGLHhJKHGJHKgjhlGIuytydfytfUGGUo65165158158S181$ui&*(") return res.status(403).send("Unauthorized.")
+    CommandLogs({
+        CommandRan: req.body.CommandRan,
+        ranat: req.body.ranat,
+        RankinGroup: req.body.RankinGroup,
+        userRan: req.body.userRan,
+        type: req.body.type
+    })
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Webhook listener running on port ${PORT}`);
+});
