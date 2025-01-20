300faae84a71933e196209055389bf7b1584b939
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-20 21:58:07 +0000

    debug

--- src/HttpsListeners/boosterget.ts
+++ src/HttpsListeners/boosterget.ts
@@ -15,12 +15,15 @@ module.exports = {
     },
     async run(client) {
         client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
+            console.log("MemberUpdate")
             if (!newMember.roles.cache.get(process.env.BoosterRole)) {
+                console.log("No Booster Role")
                 const boostersorig: Array<Booster> = await db.get("Boosters") as Array<Booster>
                 let boosters = boostersorig.filter(async r => r.UserId == await db.get(`${newMember.user.id}.verifiedRoblox`))
                 if (boosters.length > 0) await db.set("Boosters", boostersorig.filter(async r => r.UserId != await db.get(`${newMember.user.id}.verifiedRoblox`)))
             }
             else if (newMember.roles.cache.get(process.env.BoosterRole)) {
+                console.log("Yes Booster Role")
                 let boosters: Array<Booster> = await db.get("Boosters") as Array<Booster>
                 boosters = boosters.filter(async r => r.UserId == await db.get(`${newMember.user.id}.verifiedRoblox`))
                 if (boosters.length < 1) await db.push("Boosters", {UserId: await db.get(`${newMember.user.id}.verifiedRoblox`)})
