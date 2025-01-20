c8a7e315f7dd0a633c5acb45866cea075a0626be
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-20 17:52:43 +0000

    N/A

--- src/HttpsListeners/robloxVerifyDone.ts
+++ src/HttpsListeners/robloxVerifyDone.ts
@@ -7,6 +7,24 @@ module.exports = {
     directory: "/roblox-verify-complete",
     async execute(req, res, client) {
         console.log(req.query)
+        const f = await fetch("https://apis.roblox.com/oauth/v1/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams({
+                client_id: process.env.RobloxClientId as string,
+                client_secret: process.env.RobloxSecret as string,
+                grant_type: 'authorization_code',
+                code: req.query.code
+            })
+        })
+        console.log(await f.json())
+        const g = await fetch("https://apis.roblox.com/oauth/v1/userinfo", {
+            headers: {
+                Authorization: `Bearer ${(await f.json()).access_token}`
+            }
+        })
     },
     async run(client) {
     }
