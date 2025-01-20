bf4f48e9d82ed6f52935f0517bafa9bd855f0cf3
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-20 22:11:11 +0000

    remove all debug; finally got it working

--- src/HttpsListeners/boosterget.ts
+++ src/HttpsListeners/boosterget.ts
@@ -15,21 +15,14 @@ module.exports = {
     },
     async run(client) {
         client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
-            console.log("MemberUpdate")
             const roblox = await db.get(`${newMember.user.id}.verifiedRoblox`) 
             if (!newMember.roles.cache.get(process.env.BoosterRole)) {
-                console.log("No Booster Role")
                 const boostersorig: Array<Booster> = await db.get("Boosters") as Array<Booster>
                 let boosters = boostersorig.filter(r => r.UserId == roblox)
-                console.log(boosters)
-                console.log(boostersorig.filter(r => r.UserId != roblox))
-                console.log(await db.get(`${newMember.user.id}.verifiedRoblox`))
-                console.log(newMember.user.id)
                 if (boosters.length > 0) await db.set("Boosters", boostersorig.filter(r => r.UserId != roblox))
 
             }
             else if (newMember.roles.cache.get(process.env.BoosterRole)) {
-                console.log("Yes Booster Role")
                 let boosters: Array<Booster> = await db.get("Boosters") as Array<Booster>
                 boosters = boosters.filter(r => r.UserId == roblox)
                 if (boosters.length < 1) await db.push("Boosters", {UserId: await db.get(`${newMember.user.id}.verifiedRoblox`)})
