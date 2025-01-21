daf8e07ae0a647c2f2867fc34a84afedb1d7d0ea
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-21 22:22:26 +0000

    move app.use

--- httpListeners.ts
+++ httpListeners.ts
@@ -5,9 +5,9 @@ const bodypass = require("body-parser")
 const app = express();
 const PORT = 5000; // Use any available port
 
-deployListeners(app)
-
 app.use(bodypass.json())
+
+deployListeners(app)
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Webhook listener running on port ${PORT}`);
