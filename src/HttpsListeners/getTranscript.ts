3e6106de02b61aa24ca08c120a43d19a6e7f6097
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-21 17:56:37 +0000

    change return to string

--- src/HttpsListeners/getTranscript.ts
+++ src/HttpsListeners/getTranscript.ts
@@ -286,7 +286,7 @@ module.exports = {
             const channel = i.channel as TextChannel
             const transcriptfile = await transcript.createTranscript(channel, {
                 limit: -1, // Max amount of messages to fetch. `-1` recursively fetches.
-                returnType: transcript.ExportReturnType.Attachment, // Valid options: 'buffer' | 'string' | 'attachment' Default: 'attachment' OR use the enum ExportReturnType
+                returnType: transcript.ExportReturnType.String, // Valid options: 'buffer' | 'string' | 'attachment' Default: 'attachment' OR use the enum ExportReturnType
                 filename: 'transcript.html', // Only valid with returnType is 'attachment'. Name of attachment.
                 saveImages: false, // Download all images and include the image data in the HTML (allows viewing the image even after it has been deleted) (! WILL INCREASE FILE SIZE !)
                 footerText: "Exported {number} message{s}", // Change text at footer, don't forget to put {number} to show how much messages got exported, and {s} for plural
