fed1f68dbec2924a56dcbc5299088cbd3f5c4c24
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-21 22:37:35 +0000

    remove dbug

--- src/Functions/deployListeners.ts
+++ src/Functions/deployListeners.ts
@@ -4,7 +4,6 @@ export async function deployListeners(app) {
         for (const file of fs.readdirSync("src/HttpsListeners")) {
             const listener = require(`../HttpsListeners/${file}`);
             app[listener.method](listener.directory, (req, res) => {
-                console.log(req.headers)
                 if (req.headers.authorization != "wseutgghkjgigJKjklhgJHGigsfguaKgdjagLGLHhJKHGJHKgjhlGIuytydfytfUGGUo65165158158S181$ui&*(" && listener.authNeeded) return res.status(403).send("Unauthorised")
                 listener.execute(req, res)
             })
