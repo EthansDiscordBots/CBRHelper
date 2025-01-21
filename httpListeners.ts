59d6d3de4243bfe9853b4a895017889e7b9e7286
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-21 22:20:25 +0000

    yes

--- httpListeners.ts
+++ httpListeners.ts
@@ -1,10 +1,13 @@
 import { deployListeners } from "./src/Functions/deployListeners";
 const express = require("express");
+const bodypass = require("body-parser")
 
 const app = express();
 const PORT = 5000; // Use any available port
 
 deployListeners(app)
+
+app.use(bodypass.json())
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Webhook listener running on port ${PORT}`);
