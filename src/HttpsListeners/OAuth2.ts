4b986f56b22dd5127a7b2bd67a230a89a80df7ab
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-22 18:12:51 +0000

    hopefully oauth2 verif is done

--- src/HttpsListeners/OAuth2.ts
+++ src/HttpsListeners/OAuth2.ts
@@ -33,12 +33,47 @@ module.exports = {
             const {sub} = await userData.json()
             const tempKey = crypto.randomBytes(32).toString("base64")
             res.cookie("UserData", tempKey, {
-                expires: new Date(Date.now() + 60 * 60 * 1000)
+                expires: new Date(Date.now() + 60 * 60 * 1000),
+                httpOnly: true,
+                secure: true, 
+                sameSite: 'Strict',
             })
             await db.set(`verificationTokens.${tempKey}`, {
                 robloxId: sub
             })
-            res.json("hiya")
+            res.redirect("https://discord.com/oauth2/authorize?client_id=1138830931914932354&response_type=code&redirect_uri=https%3A%2F%2Fcbayr.xyz%2Foauth2%2Fcomplete&scope=identify")
+        }
+        else if (stage == "complete") {
+            const robloxdata = await db.get(`verificationTokens.${req.cookies.UserData}`)
+            const encodedCredentials = Buffer.from(`${process.env.CLIENTID}:${process.env.OAuth2Secret}`).toString('base64')
+            const requestfortoken = await fetch("https://discord.com/api/v10/oauth2/token", {
+                method: "POST",
+                body: new URLSearchParams({
+                    code: req.query.code as string,
+                    grant_type: "authorization_code",
+                    redirect_uri: "https://cbayr.xyz/oauth2"
+                }),
+                headers: {
+                    Authorization: `Basic ${encodedCredentials}`
+                }
+            })
+
+            const {access_token, token_type} = await requestfortoken.json()
+            const userData = await fetch("https://discord.com/api/v10/oauth2/@me", {
+                method: "GET",
+                headers: {
+                    Authorization: `${token_type} ${access_token}`
+                } 
+            })
+
+            const {user} = await userData.json()
+            robloxdata.discordId = user.id
+            await db.set(`verificationTokens.${req.cookies.UserData}`, robloxdata)
+
+            const userDataFull = await db.get(`verificationToken.${req.cookies.UserData}`)
+            await db.set(`${userDataFull.discordId}.verifiedRoblox`, userDataFull.robloxId)
+            await db.set(`${userDataFull.robloxId}.discordId`, userDataFull.discordId)
+            res.redirect("https://cbayr.xyz/discord")
         }
     },
     discordEvent: "ready",
