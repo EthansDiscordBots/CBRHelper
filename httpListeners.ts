3fb7f23b21038f42ed8a8815d020643e66849d41
Scr1ptxd_Ethxn <154760633+Scr1ptxdEthxn@users.noreply.github.com>
2025-01-23 16:47:35 +0000

    debug

--- httpListeners.ts
+++ httpListeners.ts
@@ -11,9 +11,10 @@ app.use(bodypass.json())
 app.use(cook())
 app.use((req, res, next) => {
     const folderPath = path.join("Website", req.path)
-
+    console.log(folderPath)
     if (folderPath) {
         res.sendFile(path.resolve(path.join(folderPath, "index.html")), err => {
+            if (err) console.log(err)
             if (err) next()
         })
     } else {
