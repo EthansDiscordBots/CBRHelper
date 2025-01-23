9999fbb0f43e25e1db53ab9dd79bb42ef3e21842
Scr1ptxd_Ethxn <154760633+Scr1ptxdEthxn@users.noreply.github.com>
2025-01-23 16:44:02 +0000

    website start

--- httpListeners.ts
+++ httpListeners.ts
@@ -2,12 +2,25 @@ import { deployListeners } from "./src/Functions/deployListeners";
 const express = require("express");
 const bodypass = require("body-parser")
 const cook = require("cookie-parser")
+import * as path from "path"
 require('dotenv').config();
 const app = express();
 const PORT = 5000; // Use any available port
 
 app.use(bodypass.json())
 app.use(cook())
+app.use((req, res, next) => {
+    const folderPath = path.join("Website", req.path)
+
+    if (folderPath) {
+        res.sendFile(path.resolve(path.join(folderPath, "index.html")), err => {
+            if (err) next()
+        })
+    } else {
+        next()
+    }
+})
+
 
 deployListeners(app)
 
