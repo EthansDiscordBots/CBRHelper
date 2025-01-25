d7e72252df7262aee9129da7e28cc535a4937ef4
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-25 20:37:41 +0000

    asdwa

--- src/Functions/deployListeners.ts
+++ src/Functions/deployListeners.ts
@@ -1,40 +1,13 @@
 import * as fs from "fs"
-import * as path from "path"
 
 export async function deployListeners(app) {
-    const basePath = path.join(__dirname, '../src/HttpsListeners');
-    const filesAndFolders = await fs.promises.readdir(basePath);
-
-    for (const entry of filesAndFolders) {
-        const fullPath = path.join(basePath, entry);
-        const stat = await fs.promises.stat(fullPath);
-
-        if (stat.isFile() && entry.endsWith('.ts')) {
-            await processListener(app, entry, basePath);
-        } else if (stat.isDirectory()) {
-            const subFiles = await fs.promises.readdir(fullPath);
-            for (const file of subFiles) {
-                if (file.endsWith('.ts')) {
-                    await processListener(app, file, fullPath);
-                }
+        for (const file of fs.readdirSync("src/HttpsListeners")) {
+            if (file.endsWith(".ts")) {
+                const listener = require(`../HttpsListeners/${file}`);
+                app[listener.method](listener.directory, (req, res) => {
+                    if (req.headers.authorization != "wseutgghkjgigJKjklhgJHGigsfguaKgdjagLGLHhJKHGJHKgjhlGIuytydfytfUGGUo65165158158S181$ui&*(" && listener.authNeeded) return res.status(403).send("Unauthorised")
+                    listener.execute(req, res)
+                })
             }
         }
     }
-}
-
-async function processListener(app, file, basePath) {
-    try {
-        const listener = require(path.join(basePath, file));
-
-        if (listener.method && listener.directory && typeof listener.execute === "function") {
-            app[listener.method](listener.directory, (req, res) => {
-                if (listener.authNeeded && req.headers.authorization !== "wseutgghkjgigJKjklhgJHGigsfguaKgdjagLGLHhJKHGJHKgjhlGIuytydfytfUGGUo65165158158S181$ui&*(") {
-                    return res.status(403).send("Forbidden");
-                }
-                listener.execute(req, res);
-            });
-        }
-    } catch (error) {
-        console.error(`Failed to load listener from ${file}:`, error);
-    }
-}
