5335bf1a3c432aa36e193306d6b2153622cedf3a
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-25 20:40:57 +0000

    asdaw

--- src/Functions/deployListeners.ts
+++ src/Functions/deployListeners.ts
@@ -9,5 +9,14 @@ export async function deployListeners(app) {
                     listener.execute(req, res)
                 })
             }
+            else {
+                for (const newfile of fs.readdirSync(`src/HttpsListeners/${file}`)) {
+                    const listener = require(`../HttpsListeners/${file}/${newfile}`)
+                    app[listener.method](listener.directory, (req, res) => {
+                        if (req.headers.authorization != "wseutgghkjgigJKjklhgJHGigsfguaKgdjagLGLHhJKHGJHKgjhlGIuytydfytfUGGUo65165158158S181$ui&*(" && listener.authNeeded) return res.status(403).send("Unauthorised")
+                        listener.execute(req, res)
+                    })
+                }
+            }
         }
     }
