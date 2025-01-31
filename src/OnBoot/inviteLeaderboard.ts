7c0f3b28c9119a3b256d5753d87838401e031d16
Scr1ptxd_Ethxn <ethan.r.armstrong@outlook.com>
2025-01-31 19:07:02 +0000

    finish invite tracking

--- src/OnBoot/inviteLeaderboard.ts
+++ src/OnBoot/inviteLeaderboard.ts
@@ -1,6 +1,6 @@
 import * as cron from "node-cron"
-import {QuickDB} from "quick.db"
-import {EmbedBuilder} from "discord.js"
+import { QuickDB } from "quick.db"
+import { EmbedBuilder } from "discord.js"
 const db = new QuickDB
 module.exports = {
     name: 'ready',
@@ -8,22 +8,47 @@ module.exports = {
     async execute(client) {
         setInterval(async () => {
             const embed = new EmbedBuilder()
-            .setColor(0x00ffe5)
+                .setColor(0x00ffe5)
 
-            const totalInvites = await db.get(`${process.env.MainServerId}.totalInvites`)
+            const totalInvites = await db.get(`${process.env.MainServerId}.totalInvites`) || {}
             const totalOrder: Array<object> = Object.entries(totalInvites).sort((a, b) => b[1].invites - a[1].invites)
             let totalString = ""
             for (let i = 0; i < 10 && i < totalOrder.length; i++) {
-                for (let i = 0; i < 10 && i < totalOrder.length; i++) {
-                    const userId = totalOrder[i][0];
-                    const invites = totalOrder[i][1]?.invites;
-                    totalString += `${i + 1}. <@${userId}>: ${invites} invites\n`;
-                }
+                const userId = totalOrder[i][0];
+                const invites = totalOrder[i][1]?.invites;
+                totalString += `${i + 1}. <@${userId}>: ${invites} invites\n`;
             }
             (await client.channels.cache.get(process.env.InviteTracking).messages.fetch(process.env.TotalInvites)).edit({
                 content: null,
                 embeds: [embed.setDescription(totalString.length > 0 ? totalString : "No tracked invites.")]
             })
+
+            const wipeInvites = await db.get(`${process.env.MainServerId}.lastWipeInvites`) || {}
+            const wipeOrder = Object.entries(wipeInvites).sort((a, b) => b[1].invites - a[1].invites)
+            totalString = ""
+            for (let i = 0; i < 10 && i < wipeOrder.length; i++) {
+                const userId = wipeOrder[i][0];
+                const invites = wipeOrder[i][1].invites;
+                totalString += `${i + 1}. <@${userId}>: ${invites} invites\n`;
+            }
+            (await client.channels.cache.get(process.env.InviteTracking).messages.fetch(process.env.WipedInvites)).edit({
+                content: null,
+                embeds: [embed.setDescription(totalString.length > 0 ? totalString : "No tracked invites.")]
+            })
+
+            const weeklyInvites = await db.get(`${process.env.MainServerId}.weeklyInvites`) || {}
+            const weeklyOrder = Object.entries(weeklyInvites).sort((a, b) => b[1].invites - a[1].invites)
+            totalString = ""
+            for (let i = 0; i < 10 && i < weeklyOrder.length; i++) {
+                const userId = weeklyOrder[i][0];
+                const invites = weeklyOrder[i][1].invites;
+                totalString += `${i + 1}. <@${userId}>: ${invites} invites\n`;
+            }
+            (await client.channels.cache.get(process.env.InviteTracking).messages.fetch(process.env.WeeklyInvites)).edit({
+                content: null,
+                embeds: [embed.setDescription(totalString.length > 0 ? totalString : "No tracked invites.")]
+            })
+
         }, 20 * 1000)
     }
 };
